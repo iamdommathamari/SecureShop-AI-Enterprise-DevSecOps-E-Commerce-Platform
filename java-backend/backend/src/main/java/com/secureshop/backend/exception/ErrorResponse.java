2cf679ad06c8adde5dca966b67c0f848bdbdf321
iamdommathamari <mohithdommathamari@gmail.com>
2026-07-01 13:54:57 +0530

    added lambok for ErrorResponse.java

--- java-backend/backend/src/main/java/com/secureshop/backend/exception/ErrorResponse.java
+++ java-backend/backend/src/main/java/com/secureshop/backend/exception/ErrorResponse.java
@@ -1,31 +1,14 @@
 package com.secureshop.backend.exception;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class ErrorResponse {
 
     private int status;
     private String message;
-
-    public ErrorResponse() {
-    }
-
-    public ErrorResponse(int status, String message) {
-        this.status = status;
-        this.message = message;
-    }
-
-    public int getStatus() {
-        return status;
-    }
-
-    public void setStatus(int status) {
-        this.status = status;
-    }
-
-    public String getMessage() {
-        return message;
-    }
-
-    public void setMessage(String message) {
-        this.message = message;
-    }
 }
