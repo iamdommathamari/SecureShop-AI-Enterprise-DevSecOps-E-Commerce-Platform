21dbd79d077614ef7ba00dc3be2133ce7c5dcff2
iamdommathamari <mohithdommathamari@gmail.com>
2026-07-01 15:57:06 +0530

    created test file and updated pom.xml

---
+++ java-backend/backend/src/test/java/com/secureshop/backend/product/ProductServiceTest.java
@@ -0,0 +1,15 @@
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+class ProductServiceTest {
+
+    @Mock
+    private ProductRepository repository;
+
+    @InjectMocks
+    private ProductServiceImpl service;
+
+}
