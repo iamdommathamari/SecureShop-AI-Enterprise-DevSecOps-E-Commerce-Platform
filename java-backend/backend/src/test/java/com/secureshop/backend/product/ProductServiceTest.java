06c2ed1a28fd2cb89e04ca055569a5279b9ad9e3
iamdommathamari <mohithdommathamari@gmail.com>
2026-07-01 16:52:04 +0530

    Created ProductServiceTest.java

--- java-backend/backend/src/test/java/com/secureshop/backend/product/ProductServiceTest.java
+++ java-backend/backend/src/test/java/com/secureshop/backend/product/ProductServiceTest.java
@@ -1,9 +1,16 @@
 package com.secureshop.backend.product;
 
+import com.secureshop.backend.exception.ProductNotFoundException;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 class ProductServiceTest {
@@ -13,5 +20,37 @@ class ProductServiceTest {
 
     @InjectMocks
     private ProductServiceImpl service;
+
+    @Test
+    void createProduct_shouldSaveProduct() {
+
+        Product product = new Product(
+            null,
+            "Laptop",
+            "Gaming Laptop",
+            85000.0
+        );
+
+        Product savedProduct = new Product(
+            1L,
+            "Laptop",
+            "Gaming Laptop",
+            85000.0
+        );
+
+        when(repository.save(product))
+            .thenReturn(savedProduct);
+
+        Product result = service.createProduct(product);
+
+        assertNotNull(result);
+
+        assertEquals(1L, result.getId());
+
+        assertEquals("Laptop", result.getName());
+
+        verify(repository, times(1))
+            .save(product);
+    }
 
 }
