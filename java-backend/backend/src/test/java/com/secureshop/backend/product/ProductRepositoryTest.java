46b9bf2815c53df26d6dcf9b51cba9a39df89cf1
iamdommathamari <mohithdommathamari@gmail.com>
2026-07-02 13:21:08 +0530

    Added pagedresponse to dto'

--- java-backend/backend/src/test/java/com/secureshop/backend/product/ProductRepositoryTest.java
+++ java-backend/backend/src/test/java/com/secureshop/backend/product/ProductRepositoryTest.java
@@ -2,11 +2,14 @@ package com.secureshop.backend.product;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 @DataJpaTest
+@AutoConfigureTestDatabase(
+        replace = AutoConfigureTestDatabase.Replace.NONE)
 class ProductRepositoryTest {
 
     @Autowired
