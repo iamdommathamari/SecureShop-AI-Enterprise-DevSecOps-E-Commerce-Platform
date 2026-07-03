aee9261f0130ffff8bfa099f445e71aec2dff75f
iamdommathamari <mohithdommathamari@gmail.com>
2026-07-03 16:06:37 +0530

    updated CatagoryRepository.java

---
+++ java-backend/backend/src/main/java/com/secureshop/backend/category/CategoryRepository.java
@@ -0,0 +1,14 @@
+package com.secureshop.backend.category;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CategoryRepository
+        extends JpaRepository<Category, Long> {
+
+    Page<Category> findByNameContainingIgnoreCase(
+            String keyword,
+            Pageable pageable);
+
+}
