c9725962fa08cfdfbf12857a6cfaf5602e4db244
iamdommathamari <mohithdommathamari@gmail.com>
2026-07-03 16:01:40 +0530

    udated CatagoryResponseDTO.java

---
+++ java-backend/backend/src/main/java/com/secureshop/backend/dto/CategoryResponseDTO.java
@@ -0,0 +1,17 @@
+package com.secureshop.backend.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class CategoryResponseDTO {
+
+    private Long id;
+
+    private String name;
+
+    private String description;
+}
