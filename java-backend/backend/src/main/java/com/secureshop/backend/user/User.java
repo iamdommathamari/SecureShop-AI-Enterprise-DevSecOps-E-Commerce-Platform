07c8bb57f132a434a1d153c7353d4876607d9ed1
iamdommathamari <mohithdommathamari@gmail.com>
2026-07-08 16:39:22 +0530

    added User.java

---
+++ java-backend/backend/src/main/java/com/secureshop/backend/user/User.java
@@ -0,0 +1,48 @@
+package com.secureshop.backend.user;
+
+import jakarta.persistence.*;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(
+        name = "users",
+        uniqueConstraints = {
+                @UniqueConstraint(
+                        name = "uk_user_email",
+                        columnNames = "email")
+        }
+)
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 100)
+    private String firstName;
+
+    @Column(nullable = false, length = 100)
+    private String lastName;
+
+    @Column(nullable = false, unique = true, length = 150)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private Role role;
+
+    @Builder.Default
+    @Column(nullable = false)
+    private Boolean enabled = true;
+}
