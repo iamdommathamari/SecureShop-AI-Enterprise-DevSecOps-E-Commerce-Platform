ee1b43c8beb0b0dab650733e7c3b4ca0c8b11f4b
iamdommathamari <mohithdommathamari@gmail.com>
2026-07-09 14:55:22 +0530

    Added UserDetailsServiceImpl.java

---
+++ java-backend/backend/src/main/java/com/secureshop/backend/security/UserDetailsServiceImpl.java
@@ -0,0 +1,29 @@
+package com.secureshop.backend.security;
+
+import com.secureshop.backend.user.User;
+import com.secureshop.backend.user.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class UserDetailsServiceImpl
+        implements UserDetailsService {
+
+    private final UserRepository repository;
+
+    @Override
+    public UserDetails loadUserByUsername(String email)
+            throws UsernameNotFoundException {
+
+        User user = repository.findByEmail(email)
+                .orElseThrow(() ->
+                        new UsernameNotFoundException(
+                                "User not found with email: " + email));
+
+        return new CustomUserDetails(user);
+    }
+}
